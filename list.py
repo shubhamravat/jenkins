7504145961abf2875fe1ee5b08fbb5e3d0a6f7f9
shubhamravat <105495494+shubhamravat@users.noreply.github.com>
2023-06-09 22:50:58 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,15 @@
+import boto3
+aws_mag_con=boto3.session.Session()
+ec2_con_cli=aws_mag_con.client("ec2")
+response=ec2_con_cli.describe_instances()
+print(response['Reservations'])
+for each_item in response['Reservations']:
+    for each_instance in each_item['Instances']:
+        print(each_instance['InstanceType'])
+        print("-----------------------------------------------")
+
+#list s3 bucket         
+
+s3_con_cli=aws_mag_con.client("s3")
+s3_response=s3_con_cli.list_buckets()
+Footer
